5ac65802851ba826ea786c90cfa73602f4c62716
mario8f <55576317+mario8f@users.noreply.github.com>
2019-09-26 09:40:47 +0200

    Add files via upload

---
+++ saludador_2.py
@@ -0,0 +1,7 @@
+def saludador_2():
+    print "Hola amigo" #Instruccion de salida
+    nombre= raw_input("Como te llamas?: ") #Instruccion de salida
+    print "Bienvenido a Python "+ nombre
+ 
+    
+saludador_2()
