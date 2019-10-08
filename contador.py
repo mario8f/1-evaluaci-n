841f0cd0598fffcffd14f731b49bf43dc5fbbac4
mario8f <55576317+mario8f@users.noreply.github.com>
2019-10-08 11:52:16 +0200

    Add files via upload

---
+++ contador.py
@@ -0,0 +1,11 @@
+#Ejemplo de for (forense)
+def ejemplo_for():
+    #i es una variable que toma los valores comprendidos en el rango
+    print "ya tu sa lo que tienes que hacer"
+    final= input("Hasta que numero: ")
+    for i in range (1,final):
+        print "i= ",i
+        
+
+ejemplo_for()
+
