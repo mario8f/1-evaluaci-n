6d0bde9c3ee1c8d48feaf3fd995d816e15e1f371
mario8f <55576317+mario8f@users.noreply.github.com>
2019-10-15 11:42:11 +0200

    Add files via upload

---
+++ par_impar_multiplode3.py
@@ -0,0 +1,17 @@
+#Identificador par e impar
+def par_impar():
+    numero=int(input("Escriba un numero "))
+    if (numero %2==0):
+        print "Es par"
+    else:
+        print "Es inpar"
+
+
+    if (numero %3==0):
+        print "Es multiplo de 3"
+    else:
+        print "No multiplo de 3"
+par_impar()
+
+
+        
