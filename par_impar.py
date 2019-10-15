4561cf74461f7273695bc2338e8fb48297c3c0d8
mario8f <55576317+mario8f@users.noreply.github.com>
2019-10-15 11:32:22 +0200

    Add files via upload

---
+++ par_impar.py
@@ -0,0 +1,11 @@
+#Identificador par e impar
+def par_impar():
+    numero=int(input("Escriba un numero "))
+    if (numero %2==0):
+        print "Es par"
+    else:
+        print "Es inpar"
+par_impar()
+
+        
+                    
