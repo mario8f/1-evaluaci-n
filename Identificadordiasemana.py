3844d240fe83b22c12a9d86fbcd116ce25aa94a2
mario8f <55576317+mario8f@users.noreply.github.com>
2019-10-22 11:39:44 +0200

    Add files via upload

---
+++ Identificadordiasemana.py
@@ -0,0 +1,21 @@
+#Identificadordiasemana
+def diasemana():
+    a=input("introduzca un dia de la semana en numero ")
+    if a==1:
+        print "Lunes"
+    if a==2:
+        print "Martes"
+    if a==3:
+        print "Miercoles"
+    if a==4:
+        print "Jueves"
+    if a==5:
+        print "Viernes"
+    if a==6:
+        print "Sabado"
+    if a==7:
+        print "Domingo"
+diasemana()
+    
+    
+    
