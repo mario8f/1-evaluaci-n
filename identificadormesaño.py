02358bbf303d5ffed56f8a123d32e3a76020b294
mario8f <55576317+mario8f@users.noreply.github.com>
2019-10-22 11:48:35 +0200

    Add files via upload

---
+++ identificadormesaño.py
@@ -0,0 +1,31 @@
+#Identificadormesaño
+def mes():
+    a=input("introduzca un mes del año ")
+    if a==1:
+        print "Enero"
+    if a==2:
+        print "Febrero"
+    if a==3:
+        print "Marzo"
+    if a==4:
+        print "Abril"
+    if a==5:
+        print "Mayo"
+    if a==6:
+        print "Junio"
+    if a==7:
+        print "Julio"
+    if a==8:
+        print "Agosto"
+    if a==9:
+        print "Septiembre"
+    if a==10:
+        print "Octubre"
+    if a==11:
+        print "Noviembre"
+    if a==12:
+        print "Diciembre"
+mes()
+    
+
+    
